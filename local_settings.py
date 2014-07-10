2a89f7bc9f36b125ac2eda284756ed2b6ce84538
kremerdesign <kremerdesign@gmail.com>
2014-07-10 11:30:23 -0700

    Initial Commit

---
+++ local_settings.py
@@ -0,0 +1,7 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'race_director',
+        }
+}
+INTERNAL_IPS = ("127.0.0.1", "10.0.2.2")
